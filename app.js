4b45e2efacb2aab1a15c04de08538206ce6c8ed0
2600 <a2600@Mac-AF.hsd1.ca.comcast.net>
2020-12-15 09:06:17 -0800

    Added

--- app.js
+++ app.js
@@ -140,11 +140,11 @@ app.get("/", function(req,res){
     res.render("garage");
 });
 
-app.get("/auth/google", 
+app.get("/home", 
     passport.authenticate('google',{ scope:["profile"] })
 );
 
-app.get("/auth/google/cars", 
+app.get("/home", 
     passport.authenticate('google', { failureRedirect: '/login' }),
     function(req, res) {
     // Successful authentication, redirect home.
